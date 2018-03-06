21dcaff9bba983a1b513898df2b9f8e961a366f0
Olga <olgaa.yatsuk@gmail.com>
2018-03-06 14:10:56 +0200

    Add responsive slider

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -6,21 +6,46 @@
 
   const options = {
     // arrows: true,
+    accessibility: false,
     appendArrows: $('.brands-slider-controls'),
     prevArrow: '<a href="#" class="arrow-link arrow-link--prev"><i class="icon-left-open-big prev"></i></a>',
     nextArrow: '<a href="#" class="arrow-link arrow-link--next"><i class="icon-right-open-big next"></i></a>',
-    // infinite: true,
-    // lazyLoad: 'progressive',
-    // pauseOnHover: true,
+    infinite: true,
+    lazyLoad: 'progressive',
+    pauseOnHover: true,
     // TODO: check for viewport (CSS class)
-    dots: true,
-    infinite: false,
     speed: 300,
     slidesToShow: 4,
     slidesToScroll: 4,
-
+    touchMove: true,
+    responsive: [
+      {
+        breakpoint: 1200,
+        settings: {
+          slidesToShow: 3,
+          slidesToScroll: 3,
+          infinite: true,
+          dots: true
+        }
+      },
+      {
+        breakpoint: 860,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 2
+        }
+      },
+      {
+        breakpoint: 560,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+          centralMode: true
+        }
+      }
+      ]
   };
 
-  // $('.js-brands-slider').slick(options);
+  $('.brands-wrapper').slick(options);
 
 }());
